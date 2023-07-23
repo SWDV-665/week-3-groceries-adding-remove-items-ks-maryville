54cb8373933b708d7b85b1d37893867c17194009
kevinsherrell <k.sherrell.1028@gmail.com>
2023-07-23 13:53:39 -0500

    assignment complete

--- groceries/src/pages/home/home.ts
+++ groceries/src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {ToastController} from "ionic-angular";
+import {AlertController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -27,13 +28,14 @@ export class HomePage {
     },
   ]
 
-
+  addItem = (obj) => {
+    this.showPrompt();
+  }
   removeItem = (obj) => {
     const toast = this.toastCtrl.create({
         message: `Removing Item - ${obj.name}`,
         duration: 1000
       }
-
     );
     toast.present();
     const isMatch = (item) => item.name === obj.name;
@@ -43,7 +45,40 @@ export class HomePage {
 
   }
 
-  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
+  showPrompt = async () => {
+    const prompt = this.alertCtrl.create({
+      title: 'Add Groceries',
+      message: "Add an item and quantity to be added to the list",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'item'
+        },
+        {
+          name: 'quantity',
+          placeholder: 'quantity'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            console.log('Save clicked', data);
+            this.items.push(data);
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController, public alertCtrl: AlertController) {
 
   }
 
